30a5312cc353b359bcb8298c1d7e6bfaefa79576
andrsantos <santosandrey146@gmail.com>
2023-04-22 13:46:25 -0300

    Primeiro commit

---
+++ Sistema de Cadastro(2)/Sistema-de-Cadastro/src/main/java/com/example/Sistema/de/Cadastro/SistemaDeCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema.de.Cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeCadastroApplication.class, args);
+	}
+
+}
